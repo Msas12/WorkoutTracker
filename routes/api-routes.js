0dcfc01240811542f9f10f8cb96723b6dc5630a0
Mike Sasiela <m5455y@gmail.com>
2021-03-20 17:19:03 -0500

    deploying to heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -41,6 +41,4 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 
-const PORT = process.env.PORT || 3001;
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-// app.use(morgan("dev"));
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
-
+  });
 
 // routes
 app.use(require("./routes/html-routes.js"))
